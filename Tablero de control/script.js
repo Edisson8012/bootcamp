f6545815d9f86d2a1c32f87365231abf95574359
Edisson8012 <92190361+Edisson8012@users.noreply.github.com>
2021-10-19 23:43:09 -0500

    Index
    Version 2

--- Tablero de control/script.js
+++ Tablero de control/script.js
@@ -1,78 +1,55 @@
-
-function ocultar_resultado(){
+/*function ocultar_resultado(){
 
     var divs = document.getElementsByClassName("div_resultado");
 
     for (var i = 0; i < divs.length; i++){
-    document.getElementsByClassName("div_resultado")[i].style.display = "none"
+        document.getElementsByClassName("div_resultado")[i].style.display = "none";
     }
 }
 
 //CALCULADORA
-/*
-var num1 = prompt("ingrese el primer número");
-var num2 = prompt("ingrese el segundo número");
-function suma(a,b){
-    var resultado =parseInt(a)+parseInt(b);
-    return resultado;
-}
-var resultado = suma(num1, num2);
-alert("el resultado de la suma es:"+ resultado);
-
-function resta(a,b){
-    resultado =parseInt(a)-parseInt(b);
-    return resultado;
-}
-resultado = resta(num1, num2);
-alert("el resultado de la resta es:"+ resultado);
-
-
-function multiplicacion(a,b){
-    resultado =parseInt(a)*parseInt(b);
-    return resultado;
-}
-resultado = multiplicacion(num1, num2);   
-alert("el resultado de la multiplicacion es:"+ resultado);
-
-
-function division(a,b){
-    if(b!=0){
-        resultado =parseInt(a)/parseInt(b);
-        return resultado;
-    }else{
-        alert("no se puede dividir por 0");
-        return resultado;
-    }
-}
-resultado = division(num1, num2);    
-alert("el resultado de la division es:"+ resultado);
 var btn_calc = document.getElementById("btn_calculadora");
 
 btn_calc.addEventListener("click", function(){
-ocultar_resultado();
-document.getElementById("div_calculadora").style.display = "block";
+    ocultar_resultado();
+    document.getElementById("div_calculadora").style.display = "block";
 });
-*/
 
 //CARACTERES
-function caracteres(){
-var texto = document.getElementById("inputCaracteres").value;
-var caracteres = texto.length; 
+var btn_contar = document.getElementById("btn_contar");
 
-document.getElementById("resultadoCaracteres").innerText=caracteres;
-}
+btn_carac.addEventListener("click", function(){
+    ocultar_resultado();
+    document.getElementById("div_caracteres").style.display = "block";
+});
+
 //Mayusculas & minusculas
-function mayuscula(){
-var texto = document.getElementByIdprompt("inputTexto");
-var texto1 = texto.toUpperCase
-var texto2 = texto.toLowerCase
-}
+var btn_may = document.getElementById("btn_may");
+btn_carac.addEventListener("click", function(){
+    ocultar_resultado();
+    document.getElementById("div_mayusculas").style.display = "block";
+});
+
 //sumatoria
-function sumatoria(){
-    var num1 = document.getElementById("inputSumatoria").value;
-    var res=0;
-    for (var i = 1; i <= num1; i++){
-        res = res + i;
-    }
-    document.getElementById("resultadoSumatoria").innerText=res;
-}
+var botonSumatoria = document.getElementById("botonSumatoria");
+btn_sum.addEventListener("click", function(){
+    ocultar_resultado();
+    document.getElementById("div_calculadora").style.display = "block";
+        });
+*/
+//CONTAR CARACTERES
+var btn_contar = document.getElementById("btn_contar");
+btn_contar.addEventListener("click", function () {
+    var a = document.getElementById("imputSumatoria").value;
+    var b = a.length;
+    document.getElementById("resultado_caracteres").innerText = "la cantidad de caracteres = " + b;
+})
+
+//MAYUSCULA & MINUSCULA
+var btn_may = document.getElementById("btn_may");
+btn_may.addEventListener("click", function() {
+    var c = document.getElementById("imputConv").innerText;
+    var d = c.toUpperCase;
+    var e = c.toLowerCase;
+    document.getElementById("resultado_may").innerText = "el texto convertido es : " + d + " " + e;
+    })
