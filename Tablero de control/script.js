7f8e2ac03e3c42ff26d2a34f73cfb27127af3478
Edisson8012 <92190361+Edisson8012@users.noreply.github.com>
2021-10-21 19:57:22 -0500

    Index
    Version 3

--- Tablero de control/script.js
+++ Tablero de control/script.js
@@ -1,8 +1,9 @@
-/*function ocultar_resultado(){
+/*
+function ocultar_resultado() {
 
     var divs = document.getElementsByClassName("div_resultado");
 
-    for (var i = 0; i < divs.length; i++){
+    for (var i = 0; i < divs.length; i++) {
         document.getElementsByClassName("div_resultado")[i].style.display = "none";
     }
 }
@@ -10,7 +11,7 @@
 //CALCULADORA
 var btn_calc = document.getElementById("btn_calculadora");
 
-btn_calc.addEventListener("click", function(){
+btn_calc.addEventListener("click", function () {
     ocultar_resultado();
     document.getElementById("div_calculadora").style.display = "block";
 });
@@ -18,38 +19,56 @@ btn_calc.addEventListener("click", function(){
 //CARACTERES
 var btn_contar = document.getElementById("btn_contar");
 
-btn_carac.addEventListener("click", function(){
+btn_contar.addEventListener("click", function () {
     ocultar_resultado();
     document.getElementById("div_caracteres").style.display = "block";
 });
 
 //Mayusculas & minusculas
 var btn_may = document.getElementById("btn_may");
-btn_carac.addEventListener("click", function(){
+btn_may.addEventListener("click", function () {
     ocultar_resultado();
     document.getElementById("div_mayusculas").style.display = "block";
 });
 
-//sumatoria
-var botonSumatoria = document.getElementById("botonSumatoria");
-btn_sum.addEventListener("click", function(){
+//Sumatoria
+var btn_sum = document.getElementById("btn_sum");
+btn_sum.addEventListener("click", function () {
     ocultar_resultado();
     document.getElementById("div_calculadora").style.display = "block";
-        });
+});
 */
 //CONTAR CARACTERES
 var btn_contar = document.getElementById("btn_contar");
 btn_contar.addEventListener("click", function () {
-    var a = document.getElementById("imputSumatoria").value;
+    var a = document.getElementById("imputcontar").value;
     var b = a.length;
     document.getElementById("resultado_caracteres").innerText = "la cantidad de caracteres = " + b;
 })
 
 //MAYUSCULA & MINUSCULA
 var btn_may = document.getElementById("btn_may");
-btn_may.addEventListener("click", function() {
+btn_may.addEventListener("click", function () {
     var c = document.getElementById("imputConv").innerText;
     var d = c.toUpperCase;
     var e = c.toLowerCase;
     document.getElementById("resultado_may").innerText = "el texto convertido es : " + d + " " + e;
-    })
+})
+
+//SUMATORIA
+function sumarNum() {
+    var su = document.getElementById("imputsum").value;
+    su = parseInt(su);
+
+    var resultado = 0;
+    for (var i = 1; i <= su; i++) {
+        resultado += i;
+    }
+    return resultado;
+}
+var sumnum = document.getElementById("btn_sum");
+sumnum.addEventListener("click", function () {
+    var res = sumarNum();
+    var cont = document.getElementById("result-sum");
+    cont.value = ("Total sumatoria: " + res);
+})
